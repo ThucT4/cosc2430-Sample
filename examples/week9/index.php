90ecbe7afbe2741bd26d8697f65c1cbe9e7e1bcd
Tri Dang <dangtrantri@gmail.com>
2021-05-04 16:47:36 +0700

    Add week 9 examples

---
+++ examples/week9/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Server Date</title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <?php
+      echo date("Y-m-d");
+    ?>
+  </body>
+</html>
