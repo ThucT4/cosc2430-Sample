458e9d1f01fe43963d32656415de9e87aeda6368
Tri Dang <dangtrantri@gmail.com>
2022-02-16 13:53:39 +0700

    remove semester specific files

--- examples/week12/stop_execution.php
+++
@@ -1,7 +0,0 @@
-<h1>This is the header</h1>
-
-<?php
-  die('The script will stop here');
-?>
-
-<h3>This is the footer</h3>
--- solutions/week9/ex1/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function get_current_time($use_12h_format = false) {
-  if ($use_12h_format) {
-    return date("h:i:s a");
-  }
-  return date("H:i:s");
-}
